b0e2bd87d9cd758eb8edbf4fc1e3c180eeabbaab
Simon <simon.kola@studenti.isissgobetti.it>
2026-02-20 12:37:57 +0100

    fix

--- video-app/app/repositories/game_repository.py
+++ video-app/app/repositories/game_repository.py
@@ -2,9 +2,7 @@ from app.db import get_db
 
 
 def get_all_games():
-    """
-    Recupera tutti i giochi da tavolo.
-    """
+    """Recupera tutti i giochi da tavolo."""
     db = get_db()
     query = """
         SELECT id, nome, numero_giocatori_massimo, durata_media, categoria
@@ -18,11 +16,9 @@ def get_all_games():
 def get_game_by_id(game_id):
     """Recupera un singolo gioco per ID."""
     db = get_db()
-    query = """
-        SELECT id, nome, numero_giocatori_massimo, durata_media, categoria
+    query = """SELECT id, nome, numero_giocatori_massimo, durata_media, categoria
         FROM giochi
-        WHERE id = ?
-    """
+        WHERE id = ?"""
     game = db.execute(query, (game_id,)).fetchone()
     if game:
         return dict(game)
